06927c3494289d100899eafabe81b75608076930
sKuangster <skuang64@gmail.com>
2025-12-27 17:01:50 -0500

    Added Residual between wealth and derivative  payoff

--- experiments/exercises.py
+++ experiments/exercises.py
@@ -38,10 +38,36 @@ def exercise_2():
     V1_down = european_option.payoff(S1_down, k)
 
     print(V1_up, V1_down)
+    return V1_up, V1_down
 
+def exercise_3():
+    print("Exercise 3: ")
+    S0 = 100
+    u = 1.2
+    d = 0.8
+    k = 50
+    S1_up = u * S0
+    S1_down = d * S0
+    r = 0.05
+    X0 = 40
+    V1_up = european_option.payoff(S1_up, k)
+    V1_down = european_option.payoff(S1_down, k)
+    residuals1 = binomial_model.replication_residuals(S0, S1_up, S1_down, r, 0, X0, V1_up, V1_down)
+    residuals2 = binomial_model.replication_residuals(S0, S1_up, S1_down, r, 1, X0, V1_up, V1_down)
+    residuals3 = binomial_model.replication_residuals(S0, S1_up, S1_down, r, -1, X0, V1_up, V1_down)
+    print(residuals1)
+    print(residuals2)
+    print(residuals3)
+    print("Average: " + str(sum(residuals1)/2))
+    print("Average: " + str(sum(residuals2)/2))
+    print("Average: " + str(sum(residuals3)/2))
+    print("Difference: " + str(residuals1[0]-residuals1[1]))
+    print("Difference: " + str(residuals2[0]-residuals2[1]))
+    print("Difference: " + str(residuals3[0]-residuals3[1]))
+    
 
 def main():
     exercise_1()
     exercise_2()
-
+    exercise_3()
 main()
--- market/binomial_model.py
+++ market/binomial_model.py
@@ -1,3 +1,4 @@
+
 """
 S0: price of stock at T = 0
 S1: price of stock at T = 1
@@ -14,3 +15,13 @@ def is_arbitrage(x1_up, x1_down):
         x1_down >= 0 and
         (x1_up > 0 or x1_down > 0)
     )
+
+def replication_residuals(S0, S1_up, S1_down, r, delta, X0, V1_up, V1_down):
+    """
+    Returns (residual_up, residual_down) where each residual is:
+    portfolio_wealth_at_time1 - derivative_payoff
+    """
+    X1_up = one_period_wealth(S0, S1_up, r, delta, X0)
+    X1_down = one_period_wealth(S0, S1_down, r, delta, X0)
+    return (X1_up-V1_up, X1_down-V1_down)
+    
